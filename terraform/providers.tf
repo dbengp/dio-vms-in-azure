56a96c3b1c5a2a73bd8a081b549708306a8f8c55
daelton pinheiro <167815532+dbengp@users.noreply.github.com>
2025-04-03 17:36:46 -0300

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "sUa_SuBsCriptioN" # Adicionando a ID da assinatura
+}
