249c1dc47ec1b03f3b432d9bd9968e583cef579a
daelton pinheiro <167815532+dbengp@users.noreply.github.com>
2025-04-03 17:40:02 -0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  default = "myResourceGroup"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "virtual_network_name" {
+  default = "myVNet"
+}
+
+variable "subnet_name" {
+  default = "mySubnet"
+}
+
+variable "public_ip_name" {
+  default = "myPublicIP"
+}
+
+variable "network_interface_name" {
+  default = "myNIC"
+}
+
+variable "virtual_machine_name" {
+  default = "myVM"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "P@$$wOrd1234!"
+}
